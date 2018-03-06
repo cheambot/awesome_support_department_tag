1423432045cdee8f20866090fd1ee55ac0481257
chsintranet <35764030+chsintranet@users.noreply.github.com>
2018-03-06 13:15:00 +0000

    Update code.php

--- code.php
+++ code.php
@@ -3,7 +3,7 @@ add_filter( 'gform_field_value_department', 'dept_id_function' );
 function dept_id_function( $value ) {
 	$user_id = get_current_user_id();
 	$user_dept = get_user_meta($user_id, 'mo_ldap_local_custom_attribute_department', true);
-    $term = get_term_by('name', $user_dept, 'department');
+	$term = get_term_by('name', $user_dept, 'department');
 	$name = $term->term_id;
 	return $name;
 }
