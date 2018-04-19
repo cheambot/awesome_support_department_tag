bc8fc145461c2a12a984e1313cbdf890776907fb
chsintranet <35764030+chsintranet@users.noreply.github.com>
2018-04-19 14:28:55 +0100

    updated to reflect that it's no longer used

--- code.php
+++ code.php
@@ -1,9 +1,12 @@
-/* Return the department tag_id from the name */
+// Return the department tag_id from the name
+// NB: This is no longer needed as the department field is now
+// a custom field and does not use the built in 'Departments' function of AS
+// 
 add_filter( 'gform_field_value_department', 'dept_id_function' );
-function dept_id_function( $value ) {
-	$user_id = get_current_user_id();
-	$user_dept = get_user_meta($user_id, 'mo_ldap_local_custom_attribute_department', true);
-	$term = get_term_by('name', $user_dept, 'department');
-	$name = $term->term_id;
-	return $name;
-}
+	function dept_id_function( $value ) {
+		$user_id = get_current_user_id();
+		$user_dept = get_user_meta($user_id, 'mo_ldap_local_custom_attribute_department', true);
+		$term = get_term_by('name', $user_dept, 'department');
+		$name = $term->term_id;
+		return $name;
+	}
